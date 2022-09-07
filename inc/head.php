e4a0aebb77f5e48b75d3bfc16aa1c10e6cf08989
Rasso Hilber <mail@rassohilber.com>
2022-09-07 16:17:46 +0200

    Update base tag

--- inc/head.php
+++ inc/head.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-  <base href="/fragments/test/">
+  <base href="/swup-fragments/test/">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
